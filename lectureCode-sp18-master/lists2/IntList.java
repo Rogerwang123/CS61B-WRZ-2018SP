8978b4c1c3f7dad8aba78ccd7bae791e322b548b
Rogerwang123 <245614069@qq.com>
2022-03-30 10:58:50 +0800

    delete eg in proj0 to execute

--- lectureCode-sp18-master/lists2/IntList.java
+++ lectureCode-sp18-master/lists2/IntList.java
@@ -34,11 +34,43 @@ public class IntList {
 		return rest.get(i - 1);
 	}
 
-	public static void main(String[] args) {
+	//disc02-Q4
+	public static IntList square(IntList L) {
+
+		if (L == null) {
+			return null;
+		} else {
+			IntList rest = square(L.rest);
+			IntList M = new IntList(L.first * L.first, rest);
+			return M;
+		}
+	}
+
+	public static IntList squareMutative(IntList L) {
+		if (L == null) {
+			return null;
+		} else {
+			L.first = L.first * L.first;
+			squareMutative(L.rest);
+
+		}
+		return L;
+	}
+
+	public static IntList squareIter(IntList L) {
+		if (L == null) {
+			return L;
+		}
+		
+	}
+
+
+		public static void main(String[] args) {
 		IntList L = new IntList(15, null);
 		L = new IntList(10, L);
 		L = new IntList(5, L);
-
+		IntList N = squareMutative(L);
+		IntList M = square(L);
 		System.out.println(L.get(100));
 	}
 } 
